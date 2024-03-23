941c2e422f6914c3cc3694cf430798ec41bcd243
joaovitor32 <joaovitormunizlopes@gmail.com>
2024-03-23 19:16:23 -0300

    refactor: add changes on card

--- src/components/Card/style.ts
+++ src/components/Card/style.ts
@@ -1,10 +1,11 @@
 const style = `
   #card {
-    width: 50%;
+    width: 50vw;
     height: 50%;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
     border-radius: 8px;
     background-color: rgba(128, 128, 128, 0.24);
+    padding: 16px 24px;
   }
 `;
 
